71183c9ddfcf4716a1a3661061bce6ef34cdc859
goretcmma-unix <goretcmma@gmail.com>
2026-07-30 19:45:56 +0300

    finalv1.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { LanguageProvider } from './i18n.tsx'
 
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual'
+}
+window.scrollTo(0, 0)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>

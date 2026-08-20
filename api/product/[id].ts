da0ade1be7f389ac7bc5938682da453d67974b53
goretcmma-unix <goretcmma@gmail.com>
2026-08-20 05:19:47 +0300

    fix: clean favicon tags in SSR product endpoint for Google indexing

--- api/product/[id].ts
+++ api/product/[id].ts
@@ -118,11 +118,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     <meta name="twitter:title" content="${esc(title)}" />
     <meta name="twitter:description" content="${esc(description)}" />
     <meta name="twitter:image" content="${imageUrl}" />
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96.png" />
-    <link rel="icon" type="image/png" sizes="144x144" href="/favicon-144.png" />
-    <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192.png" />
     <link rel="apple-touch-icon" sizes="192x192" href="/favicon-192.png" />
     <meta name="theme-color" content="#cf9b41" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />

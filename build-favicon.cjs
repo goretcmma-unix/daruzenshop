c6277ad5d890e314ec8809a7743423f22dcb82a2
goretcmma-unix <goretcmma@gmail.com>
2026-08-24 22:34:56 +0300

    favicon: gradient bg + white logo + no border

--- build-favicon.cjs
+++ build-favicon.cjs
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const sharp = require('sharp');
-const svg = fs.readFileSync('public/favicon.svg', 'utf8');
+const path = require('path');
+const dir = path.dirname(require.main.filename);
+const svgPath = path.join(dir, 'public', 'favicon.svg');
+const svg = fs.readFileSync(svgPath, 'utf8');
 const buf = Buffer.from(svg, 'utf8');
 const sizes = [16,32,48,96,120,144,180,192,512];
-Promise.all(sizes.map(s => sharp(buf).resize(s,s).png().toFile('public/favicon-'+s+'.png')))
-  .then(() => sharp(buf).resize(512,512).png().toFile('public/favicon.png'))
-  .then(() => { fs.copyFileSync('public/favicon-48.png', 'public/favicon.ico'); console.log('all done'); });
+Promise.all(sizes.map(s => sharp(buf).resize(s,s).png().toFile(path.join(dir, 'public', 'favicon-'+s+'.png'))))
+  .then(() => sharp(buf).resize(512,512).png().toFile(path.join(dir, 'public', 'favicon.png')))
+  .then(() => { fs.copyFileSync(path.join(dir, 'public', 'favicon-48.png'), path.join(dir, 'public', 'favicon.ico')); console.log('all done from ' + dir); });

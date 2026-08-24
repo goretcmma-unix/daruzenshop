da00142702069c87c98e75c4b90a3be6fc72c520
goretcmma-unix <goretcmma@gmail.com>
2026-08-25 02:12:26 +0300

    remove unrecognized language attribute from google feed

--- api/google-feed.ts
+++ api/google-feed.ts
@@ -95,7 +95,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
       xml += '<item>\n';
       xml += '<g:id>' + esc(p.id + '-' + lang) + '</g:id>\n';
-      xml += '<g:content_language>' + lang + '</g:content_language>\n';
       xml += '<g:title>' + esc(seoName) + '</g:title>\n';
       xml += '<g:description>' + esc(desc) + '</g:description>\n';
       xml += '<g:link>' + esc(url) + '</g:link>\n';
